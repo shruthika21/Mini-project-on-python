6fdd739a53caf229c516437dc38c1b5d5d65441a
shruthika21 <160160832+shruthika21@users.noreply.github.com>
2024-02-25 23:58:09 +0530

    sudoku solver.py

---
+++ sudoku solver.py
@@ -0,0 +1,78 @@
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print("| ", end="")
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def find_empty_location(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return (i, j)
+    return None
+
+def is_valid(board, num, position):
+    # Check row
+    for i in range(9):
+        if board[position[0]][i] == num and position[1] != i:
+            return False
+
+    # Check column
+    for i in range(9):
+        if board[i][position[1]] == num and position[0] != i:
+            return False
+
+    # Check square
+    square_x = position[1] // 3
+    square_y = position[0] // 3
+    for i in range(square_y * 3, square_y * 3 + 3):
+        for j in range(square_x * 3, square_x * 3 + 3):
+            if board[i][j] == num and (i, j) != position:
+                return False
+
+    return True
+
+def solve(board):
+    empty = find_empty_location(board)
+    if not empty:
+        return True
+    else:
+        row, col = empty
+
+    for i in range(1, 10):
+        if is_valid(board, i, (row, col)):
+            board[row][col] = i
+            if solve(board):
+                return True
+            board[row][col] = 0
+
+    return False
+
+if _name_ == "_main_":
+    # Example Sudoku board (0 represents empty cells)
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    print("Original Sudoku Board:")
+    print_board(board)
+
+    if solve(board):
+        print("\nSolution:")
+        print_board(board)
+    else:
+        print("\nNo solution exists.")
